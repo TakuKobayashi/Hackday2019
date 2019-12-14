810be549ee12f2ca7907adde55f854548ceb3e4e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-12-15 03:35:31 +0900

    雑にだけどLinePayまで実装してみた

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,21 +4,28 @@ import './App.css';
 import { Product } from './interfaces/product'
 import { Card, Pagination } from 'react-rainbow-components';
 import axios from 'axios';
+import {Button, Modal} from "react-rainbow-components/components";
+import linepay_icon from './icons/linepay_logo.png'
+const QRCode = require('qrcode.react');
 
-interface PagingState {
+interface ProductsState {
   activePage: number;
   products: Product[],
+  isOpen: boolean,
 }
 
-class App extends React.Component<{}, PagingState> {
+class App extends React.Component<{}, ProductsState> {
   constructor(props: any) {
     super(props);
     this.state = {
       activePage: 1,
       products: [],
+      isOpen: false,
     };
     this.handleOnChange = this.handleOnChange.bind(this);
     this.loadContents();
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleOnClose = this.handleOnClose.bind(this);
   }
 
   async loadContents(): Promise<Product[]> {
@@ -35,14 +42,47 @@ class App extends React.Component<{}, PagingState> {
     const firstItem = lastItem - 2;
     return this.state.products.slice(firstItem, lastItem).map((product) => (
       <Card
-        key={product.name}
+        title={product.name}
         style={{ width: 240 }}
         className="rainbow-m-bottom_x-large rainbow-m-right_small"
-        footer={<span className="rainbow-font-size-text_large rainbow-color_dark-1">{product.name}</span>}
+        footer={
+          <div>
+            <div className="rainbow-font-size-text_large rainbow-color_dark-1">{product.price} {product.currency}</div>
+            <Button
+              label="購入する"
+              onClick={this.handleOnClick}
+              variant="brand"
+              className="rainbow-m-around_medium"
+            />
+          </div>
+        }
       >
         <div style={{width: '100%', height: 240, backgroundImage: `url(${product.image_url})`, backgroundSize: 'cover'}} />
+        <Modal
+            id="modal-1"
+            title={product.name}
+            isOpen={this.state.isOpen}
+            onRequestClose={this.handleOnClose}
+        >
+          <div className="rainbow-align-content_center">
+            <img
+              src={linepay_icon}
+            />
+          </div>
+          <div>
+            <QRCode value={process.env.REACT_APP_API_ROOT_URL + '/pay/product/' + product.id} />
+          </div>
+        </Modal>
       </Card>
     ));
+  }
+
+  handleOnClick() {
+    return this.setState({ isOpen: true });
+  }
+
+  handleOnClose() {
+    return this.setState({ isOpen: false });
   }
 
   handleOnChange(event: React.MouseEvent<HTMLElement>, page: number) {
